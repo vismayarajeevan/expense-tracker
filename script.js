d269d10c143d6eed9501d35762acff97d831aa2d
vismayarajeevan <vismayarajeevan15@gmail.com>
2024-10-26 22:54:32 +0530

    home page created

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle(){
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popup=document.getElementsByClassName('popup')[0];
+    popup.classList.toggle('active');
+
+    
+
+}
---
+++ script1.js
@@ -0,0 +1,17 @@
+function toggle(){
+    
+
+    var popup=document.getElementsByClassName('popup')[0];
+    popup.classList.toggle('active');
+
+    
+
+}
+function type(){
+    document.write("jnhhvb nmnmcbmm");
+    
+
+}
+
+
+
